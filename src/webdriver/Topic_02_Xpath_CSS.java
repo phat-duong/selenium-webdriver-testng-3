7b13fce8b59822deec52faff983d4c7dbaa302fe
Duong Tan Phat <phat.duong.henry@gmail.com>
2023-04-22 10:48:13 +0700

    add topic 02

---
+++ src/webdriver/Topic_02_Xpath_CSS.java
@@ -0,0 +1,18 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_02_Xpath_CSS {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+
+
+}
